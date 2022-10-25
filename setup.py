ced8462056e515908a04922a7a312bc241e0e96d
WillDudley <Will2346@live.co.uk>
2022-10-25 10:48:04 +0100

    attempted setup

--- setup.py
+++ setup.py
@@ -1,92 +1,48 @@
-"""Sets up the Kabuki module."""
-
 import os
-from setuptools import find_packages, setup, Extension
-from numpy import get_include
-from Cython.Build import cythonize
 
+from setuptools import setup, Extension, find_packages
 
 os.environ['CFLAGS'] = '-std=c++11'
 
-# setup Cython build
-ext = Extension('kabuki.dataset',
-                sources=['kabuki/datastructure/dataset.pyx'],
-                include_dirs=[get_include(), 'kabuki/datastructure/cpp/include'],
-                language='c++',
-                extra_compile_args=["-std=c++11", "-O3", "-ffast-math"],
-                extra_link_args=["-std=c++11"])
+if __name__ == "__main__":
+    from numpy import get_include
+    from Cython.Build import cythonize
 
-ext_modules = cythonize([ext],
-                        compiler_directives={
-                            'linetrace': True,
-                            'binding': True
-                        })
+    # setup Cython build
+    ext = Extension('kabuki.datastructure.dataset',
+                    sources=['kabuki/datastructure/dataset.pyx'],
+                    include_dirs=[get_include(), 'kabuki/datastructure/cpp/include'],
+                    language='c++',
+                    extra_compile_args=["-std=c++11", "-O3", "-ffast-math"],
+                    extra_link_args=["-std=c++11"])
 
+    ext_modules = cythonize([ext],
+                            compiler_directives={
+                                'linetrace': True,
+                                'binding': True
+                            })
 
-def get_description():
-    """Gets the description from the readme."""
-    with open("README.md") as fh:
-        long_description = ""
-        header_count = 0
-        for line in fh:
-            if line.startswith("##"):
-                header_count += 1
-            if header_count < 2:
-                long_description += line
-            else:
-                break
-    return header_count, long_description
-
-
-def get_version():
-    """Gets the kabuki version."""
-    path = "kabuki/__init__.py"
-    with open(path) as file:
-        lines = file.readlines()
-
-    for line in lines:
-        if line.startswith("__version__"):
-            return line.strip().split()[-1].strip().strip('"')
-    raise RuntimeError("bad version data in __init__.py")
-
-
-extras = {"robotics": ["gymnasium-robotics==1.0.1"]}
-
-extras["all"] = (
-    extras["robotics"]
-)
-
-version = get_version()
-header_count, long_description = get_description()
-
-setup(
-    name="Kabuki",
-    version=version,
-    author="Farama Foundation",
-    author_email="contact@farama.org",
-    description="Gymnasium for offline reinforcement learning",
-    url="https://kabuki.farama.org/",
-    license_files=("LICENSE.txt",),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    keywords=["Reinforcement Learning", "game", "RL", "AI", "gymnasium"],
-    python_requires=">=3.7, <3.11",
-    packages=["kabuki"] + ["kabuki." + pkg for pkg in find_packages("kabuki")],
-    include_package_data=True,
-    install_requires=["numpy>=1.18.0", "gymnasium>=0.26.0"],
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    extras_require=extras,
-    package_data={'d3rlpy': ['*.pyx',
-                             '*.pxd',
-                             '*.h',
-                             '*.pyi',
-                             'py.typed']},
-    ext_modules=ext_modules
-)
+    # main setup
+    setup(name="kabuki",
+          version="0.0.1",
+          description="An offline deep reinforcement learning library",
+          long_description=open("README.md").read(),
+          long_description_content_type="text/markdown",
+          url="https://github.com/takuseno/d3rlpy",
+          author="Takuma Seno",
+          author_email="takuma.seno@gmail.com",
+          license="MIT License",
+          classifiers=["Development Status :: 2 - Pre-Alpha",
+                       "Programming Language :: Python :: 3.6",
+                       "Programming Language :: Python :: 3.7",
+                       "Programming Language :: Python :: 3.8",
+                       "Programming Language :: Python :: Implementation :: CPython"],
+          install_requires=["numpy>=1.18.0"],
+          packages=find_packages(exclude=["tests*"]),
+          python_requires=">=3.7.0",
+          package_data={'d3rlpy': ['*.pyx',
+                                   '*.pxd',
+                                   '*.h',
+                                   '*.pyi',
+                                   'py.typed']},
+          ext_modules=ext_modules)
