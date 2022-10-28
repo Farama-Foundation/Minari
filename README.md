cd6472d511f5a84a2b43b762b075d3a491c901a5
will <will2346@live.co.uk>
2022-10-28 23:46:04 +0100

    CI

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 <p align="center">
     <img src="kabuki-text.png" width="500px"/>
 </p>
-Kabuki is intended to be a Python library for conducting research in offline reinforcement learning, akin to an offline version of Gymnasium or an offline RL version of HugginFace's datasets library. The goal is to release a fully working beta in late November or early December.
+Kabuki is intended to be a Python library for conducting research in offline reinforcement learning, akin to an offline version of Gymnasium or an offline RL version of HuggingFace's datasets library. The goal is to release a fully working beta in late November or early December.
 
 We have a public discord server (which we also use to coordinate development work) that you can join here: https://discord.gg/jfERDCSw if you're interested in it.
+
+
+## Saving to MDPDatasets
+It is not the aim of Kabuki to insist that you use a certain buffer implementation. However, in order to maintain standardisation across the library, we have a standardised format, the `MDPDataset` class, for saving replay buffers to file. 
+
---
+++ tutorials/basic_usage/saving_to_mdpdataset.py
@@ -0,0 +1,40 @@
+import gymnasium as gym
+from kabuki.dataset import MDPDataset
+import numpy as np
+
+num_episodes = 10
+
+env = gym.make("LunarLander-v2", render_mode="human")
+observation, info = env.reset(seed=42)
+
+replay_buffer = {
+    "episode": np.array([]),
+    "observation": np.array([]),
+    "action": np.array([]),
+    "reward": np.array([]),
+    "done": np.array([]),
+}
+
+for episode in range(num_episodes):
+    observation, info = env.reset()
+    terminated = False
+    truncated = False
+    while not terminated and not truncated:
+        action = env.action_space.sample()  # User-defined policy function
+        observation, reward, terminated, truncated, info = env.step(action)
+        np.append(replay_buffer["episode"], episode)
+        np.append(replay_buffer["observation"], observation)
+        np.append(replay_buffer["action"], action)
+        np.append(replay_buffer["reward"], reward)
+        np.append(replay_buffer["done"], terminated)
+
+env.close()
+
+ds = MDPDataset(
+    observations=replay_buffer["observation"],
+    actions=replay_buffer["action"],
+    rewards=replay_buffer["reward"],
+    terminals=replay_buffer["done"],
+)
+
+ds.dump("test_dataset.hdf5")
