f0564631bd73cd6297363793a9d08f71503f099a
will <will2346@live.co.uk>
2022-10-29 18:39:58 +0100

    local retrieve if poss

--- kabuki/hosting/hosting.py
+++ kabuki/hosting/hosting.py
@@ -1,3 +1,5 @@
+import os.path
+
 from google.cloud import storage
 from ..utils.assert_name_spec import test_and_return_name
 
@@ -17,23 +19,27 @@ def upload_dataset(dataset_path: str):
 
 
 def retrieve_dataset(source_blob_name: str, download=True, return_dataset=True):
-    project_id = "dogwood-envoy-367012"
-    bucket_name = "kabuki-datasets"
-    storage_client = storage.Client(project=project_id)
+    if os.path.isfile(source_blob_name):
+        return source_blob_name
+    else:
+        print("Dataset not found locally. Downloading from Farama servers...")
+        project_id = "dogwood-envoy-367012"
+        bucket_name = "kabuki-datasets"
+        storage_client = storage.Client(project=project_id)
 
-    bucket = storage_client.bucket(bucket_name)
+        bucket = storage_client.bucket(bucket_name)
 
-    # Construct a client side representation of a blob.
-    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
-    # any content from Google Cloud Storage. As we don't need additional data,
-    # using `Bucket.blob` is preferred here.
-    blob = bucket.blob(source_blob_name)
+        # Construct a client side representation of a blob.
+        # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+        # any content from Google Cloud Storage. As we don't need additional data,
+        # using `Bucket.blob` is preferred here.
+        blob = bucket.blob(source_blob_name)
 
-    if download:
-        blob.download_to_filename(source_blob_name)
-        print(f"File {source_blob_name} downloaded!")
-    if return_dataset:
-        return blob
+        if download:
+            blob.download_to_filename(source_blob_name)
+            print(f"Dataset {source_blob_name} downloaded!")
+        if return_dataset:
+            return blob
 
 
 def list_datasets():
