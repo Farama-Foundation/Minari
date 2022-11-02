ff9eed71bc185d9c19d01c2eb16444b1e6a8ed47
will <will2346@live.co.uk>
2022-11-02 23:17:28 +0000

    rename

--- kabuki/__init__.py
+++ kabuki/__init__.py
@@ -1,4 +1,4 @@
 from . import dataset
-from kabuki.hosting.hosting import upload_dataset, retrieve_dataset, list_datasets
+from kabuki.storage.hosting import upload_dataset, retrieve_dataset, list_datasets
 
 __version__ = "0.0.1"
