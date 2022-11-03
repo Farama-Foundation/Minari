45185c55703b5e899cb7866b53472cb44c8c32ab
will <will2346@live.co.uk>
2022-11-03 13:53:58 +0000

    delete unnecessary files

--- kabuki/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import dataset
-from kabuki.storage.hosting import (
-    upload_dataset,
-    download_dataset,
-    list_remote_datasets,
-)
-from kabuki.storage.local import load_dataset, list_local_datasets, delete_dataset
-
-__version__ = "0.0.1"
