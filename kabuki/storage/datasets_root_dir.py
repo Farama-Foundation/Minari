fe007edd4c60193da59230377902b6e753ee0839
will <will2346@live.co.uk>
2022-11-03 13:56:42 +0000

    Revert "delete unnecessary files"
    This reverts commit 45185c55703b5e899cb7866b53472cb44c8c32ab.

---
+++ kabuki/storage/datasets_root_dir.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+
+def get_file_path(dataset_name):
+    """Get the path to a dataset file."""
+    # IF YOU EDIT THIS PLEASE ALSO EDIT LINES ~540 IN dataset.pyx
+    datasets_path = os.environ.get("KABUKI_DATASETS_PATH")
+    if datasets_path is not None:
+        file_path = os.path.join(datasets_path, f"{dataset_name}.hdf5")
+    else:
+        datasets_path = os.path.join(os.path.expanduser("~"), ".kabuki", "datasets")
+        file_path = os.path.join(datasets_path, f"{dataset_name}.hdf5")
+
+    os.makedirs(datasets_path, exist_ok=True)
+    return Path(file_path)
---
+++ kabuki/utils/assert_name_spec.py
@@ -0,0 +1,23 @@
+import re
+
+
+def test_and_return_name(dataset_path):
+    parsed = re.search(
+        r"(\\*|\/*)([\w\-]*)\.*(\w*)$", dataset_path
+    )  # https://regex101.com/r/cg5fK7/1
+    if not parsed:
+        raise ValueError(
+            f"Invalid dataset path {dataset_path}. Please raise an issue on GitHub."
+        )
+    filename = parsed.group(2)
+    extension = parsed.group(3)
+    if extension == "":
+        extension = "hdf5"
+    assert (
+        extension == "hdf5"
+    ), f"File extension must be .hdf5, not {extension}. (Are you trying to use a period in your dataset name?), {parsed.groups()}"
+    if not re.match(r"\w+-v\d-\w+$", filename):
+        raise ValueError(
+            f"Invalid dataset name {filename}. Must be in the format <environment_name>-v<environment_version>-<description>."
+        )
+    return filename
