3f90b924e2a5be6c9d398d8c19e05ad5bcd4498e
will <will2346@live.co.uk>
2022-12-11 01:38:29 +0000

    add environment_name

--- tutorials/RemoteStorage/remote_storage.py
+++ tutorials/RemoteStorage/remote_storage.py
@@ -76,6 +76,7 @@ def generate_dataset(dataset_name: str):
     ds = MinariDataset(
         dataset_name=dataset_name,
         algorithm_name="random_policy",
+        environment_name="LunarLander-v2",
         seed_used=42,  # For the simplicity of this example, we're not actually using a seed.
         code_permalink="https://github.com/Farama-Foundation/Kabuki/blob/f095bfe07f8dc6642082599e07779ec1dd9b2667/tutorials/LocalStorage/local_storage.py",
         author="WillDudley",
