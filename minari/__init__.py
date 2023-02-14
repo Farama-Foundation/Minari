d6d70a3dade3463471216a959b531a2f2bf93ff2
Rodrigo de Lazcano Perez-Vicente <rlazcano@umd.edu>
2023-02-14 15:58:29 -0500

    update hosting funcs

--- minari/__init__.py
+++ minari/__init__.py
@@ -1,8 +1,5 @@
-from minari.storage.hosting import (
-    download_dataset,
-    list_remote_datasets,
-    upload_dataset,
-)
-from minari.storage.local import delete_dataset, list_local_datasets, load_dataset
+from minari.minari_dataset import create_dataset_from_collector_env, create_dataset_from_buffers, combine_datasets
+from minari.storage.local import load_dataset, list_local_datasets, delete_dataset
+from minari.storage.hosting import list_remote_datasets, upload_dataset, download_dataset
 
 __version__ = "0.0.1"
