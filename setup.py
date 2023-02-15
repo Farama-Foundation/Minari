f3fd29a84770239ee7c9376cf87cec3697c84b25
Rodrigo de Lazcano Perez-Vicente <rlazcano@umd.edu>
2023-02-15 17:16:00 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,37 @@
-from setuptools import find_packages, setup
+"""Setups the project."""
 
-if __name__ == "__main__":
-    setup(
-        name="minari",
-        version="0.2.2",
-        description="Datasets for offline deep reinforcement learning",
-        long_description=open("README.md").read(),
-        long_description_content_type="text/markdown",
-        url="https://github.com/Farama-Foundation/Minari",
-        author="Will Dudley",
-        author_email="will2346@live.co.uk",
-        license="MIT License",
-        classifiers=[
-            "Development Status :: 2 - Pre-Alpha",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Python :: 3.11",
-        ],
-        install_requires=[
-            "numpy>=1.18.0",
-            "h5py==3.7.0",
-            "structlog==22.1.0",
-            "tensorboardX==2.4",
-            "typing_extensions==4.4.0",
-            "google-cloud-storage==2.5.0",
-            "protobuf==3.20.1",
-            "gymnasium @ git+https://github.com/WillDudley/Gymnasium.git@spec_stack",
-        ],
-        packages=find_packages(exclude=["tests*"]),
-        python_requires=">=3.7.0",
-        # package_data={"minari": ["*.pyx", "*.pxd", "*.h", "*.pyi", "py.typed"]},
-    )
+import pathlib
+
+from setuptools import setup
+
+
+CWD = pathlib.Path(__file__).absolute().parent
+
+
+def get_version():
+    """Gets the gymnasium version."""
+    path = CWD / "minari" / "__init__.py"
+    content = path.read_text()
+
+    for line in content.splitlines():
+        if line.startswith("__version__"):
+            return line.strip().split()[-1].strip().strip('"')
+    raise RuntimeError("bad version data in __init__.py")
+
+
+def get_description():
+    """Gets the description from the readme."""
+    with open("README.md") as fh:
+        long_description = ""
+        header_count = 0
+        for line in fh:
+            if line.startswith("##"):
+                header_count += 1
+            if header_count < 2:
+                long_description += line
+            else:
+                break
+    return long_description
+
+
+setup(name="minari", version=get_version(), long_description=get_description())
