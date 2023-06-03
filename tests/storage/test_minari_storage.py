514d7df5cd873622a9581fb42546639c8dcda8dc
Shreyans Jain <shreyansjain@Shreyanss-MacBook-Air.local>
2023-06-04 00:45:14 +0530

    made some pre-commit corrections

--- tests/storage/test_minari_storage.py
+++ tests/storage/test_minari_storage.py
@@ -1,12 +1,7 @@
 import os
-import shutil
 
-import gymnasium as gym
 import h5py
-import numpy as np
 import pytest
-from gymnasium import spaces
-from gymnasium.envs.registration import register
 
 from minari.dataset.minari_storage import MinariStorage
 
@@ -17,24 +12,28 @@ file_path = os.path.join(os.path.expanduser("~"), ".minari", "datasets")
 def _create_dummy_dataset(file_path):
 
     os.makedirs(file_path, exist_ok=True)
-    
-    with h5py.File(os.path.join(file_path,'dummy-test-v0.hdf5'), 'w') as f:
 
-        f.attrs['flatten_observation'] = False
-        f.attrs['flatten_action'] = False
-        f.attrs['env_spec'] = r"""{"id": "DummyEnv-v0", "entry_point": "dummymodule:dummyenv", "reward_threshold": null, "nondeterministic": false, "max_episode_steps": 300, "order_enforce": true, "autoreset": false, "disable_env_checker": false, "apply_api_compatibility": false, "kwargs": {"reward_type": "dense"}, "additional_wrappers": [], "vector_entry_point": null}"""
-        f.attrs['total_episodes'] = 100
-        f.attrs['total_steps'] = 1000
-        f.attrs['dataset_id'] = 'dummy-test-v0'
+    with h5py.File(os.path.join(file_path, "dummy-test-v0.hdf5"), "w") as f:
+
+        f.attrs["flatten_observation"] = False
+        f.attrs["flatten_action"] = False
+        f.attrs[
+            "env_spec"
+        ] = r"""{"id": "DummyEnv-v0", "entry_point": "dummymodule:dummyenv", "reward_threshold": null, "nondeterministic": false, "max_episode_steps": 300, "order_enforce": true, "disable_env_checker": false, "apply_api_compatibility": false, "additional_wrappers": []}"""
+        f.attrs["total_episodes"] = 100
+        f.attrs["total_steps"] = 1000
+        f.attrs["dataset_id"] = "dummy-test-v0"
 
 
 def test_minari_storage_missing_env_module():
-    
+
     file_path = os.path.join(os.path.expanduser("~"), ".minari", "datasets")
-    
+
     _create_dummy_dataset(file_path)
-    
-    with pytest.raises(ModuleNotFoundError, match='Install dummymodule for loading DummyEnv-v0 data'):
+
+    with pytest.raises(
+        ModuleNotFoundError, match="Install dummymodule for loading DummyEnv-v0 data"
+    ):
         MinariStorage(os.path.join(file_path, "dummy-test-v0.hdf5"))
 
-    os.remove(os.path.join(file_path,'dummy-test-v0.hdf5'))
+    os.remove(os.path.join(file_path, "dummy-test-v0.hdf5"))
