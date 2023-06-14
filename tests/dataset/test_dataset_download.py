ca5a4c817899ab5de4077f0d2e7c820968166ba5
Shreyans Jain <jshrey8@gmail.com>
2023-06-15 03:33:53 +0530

    Added force_download flag for downloading locally existing datasets (#90)
    * added force_download flag for downloading locally existing datasets
    
    * added custom error and unit tests for existing datasets
    
    * made some pre-commit corrections
    
    * Revert "made some pre-commit corrections"
    
    This reverts commit e15af9b6d4dd70d230cd8b5063d9f4b55f8ac4da.
    
    * Revert "added custom error and unit tests for existing datasets"
    
    This reverts commit 2a01837db352b926d900212709eb860417c38945.
    
    * updated warning message
    
    * added tests for warnings and none return, removed additional warnings
    
    ---------
    
    Co-authored-by: Shreyans Jain <shreyansjain@Shreyanss-MacBook-Air.local>

--- tests/dataset/test_dataset_download.py
+++ tests/dataset/test_dataset_download.py
@@ -2,6 +2,7 @@ import pytest
 
 import minari
 from minari import MinariDataset
+from minari.storage.datasets_root_dir import get_dataset_path
 
 
 @pytest.mark.parametrize(
@@ -19,9 +20,20 @@ def test_download_dataset_from_farama_server(dataset_id: str):
     remote_datasets = minari.list_remote_datasets()
     assert dataset_id in remote_datasets
 
-    minari.download_dataset(dataset_id)
+    minari.download_dataset(dataset_id, force_download=True)
     local_datasets = minari.list_local_datasets()
     assert dataset_id in local_datasets
+
+    file_path = get_dataset_path(dataset_id)
+
+    with pytest.warns(
+        UserWarning,
+        match=f"Skipping Download. Dataset {dataset_id} found locally at {file_path}, Use force_download=True to download the dataset again.\n",
+    ):
+        minari.download_dataset(dataset_id)
+
+    download_dataset_output = minari.download_dataset(dataset_id)
+    assert download_dataset_output is None
 
     dataset = minari.load_dataset(dataset_id)
     assert isinstance(dataset, MinariDataset)
