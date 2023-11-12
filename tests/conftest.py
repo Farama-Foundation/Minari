3d8742c947ddff71ac5ac6b7c36e5e06626b4c0f
Alex Davey <alexdavey0@gmail.com>
2023-11-12 01:47:16 +0000

    address review comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,14 @@
-import os
 import tempfile
 
 import pytest
+from pytest import MonkeyPatch
 
 
 @pytest.fixture(autouse=True)
 def tmp_dataset_dir():
     """Generate a temporary directory for Minari datasets."""
     tmp_dir = tempfile.TemporaryDirectory()
-    os.environ["MINARI_DATASETS_PATH"] = tmp_dir.name
-    yield tmp_dir.name
+    with MonkeyPatch.context() as mp:
+        mp.setenv("MINARI_DATASETS_PATH", tmp_dir.name)
+        yield tmp_dir.name
     tmp_dir.cleanup()
