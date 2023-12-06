f1b2ca0ddaa1c563a774b0dc5bc3b821cf97e49a
Omar Younis <omar.younis98@gmail.com>
2023-12-06 00:16:31 -0500

    fix datacollector deprecation

--- minari/__init__.py
+++ minari/__init__.py
@@ -1,3 +1,4 @@
+import warnings
 from minari.data_collector import DataCollector
 from minari.data_collector.callbacks import EpisodeMetadataCallback, StepDataCallback
 from minari.dataset.minari_dataset import EpisodeData, MinariDataset
@@ -43,5 +44,9 @@ __version__ = "0.4.2"
 
 def __getattr__(name):
     if name == "DataCollectorV0":
+        warnings.simplefilter('once', DeprecationWarning)
         from minari.data_collector import DataCollectorV0
         return DataCollectorV0
+    else:
+        raise ImportError(f"cannot import name '{name}' from '{__name__}' ({__file__})")
+    
