73e77117f698417bd826d817f12d73c02dc5da2b
Caleb McIrvin <calebmcirvin111@vt.edu>
2024-03-12 10:33:21 -0400

    AgileRL and TorchRL smoke tests (#196)
    * Added AgileRL integration tests
    
    * Cleaned up AgileRL testing logic, added pytest fixtures
    
    * Added TorchRL integration test
    
    * Running pre-commit hooks
    
    * Added dependencies

---
+++ tests/integrations/test_agile_rl.py
@@ -0,0 +1,62 @@
+import gymnasium as gym
+import h5py
+import pytest
+from agilerl.components.replay_buffer import ReplayBuffer
+from agilerl.utils.minari_utils import MinariToAgileBuffer, MinariToAgileDataset
+
+import minari
+from minari import DataCollector
+from tests.common import create_dummy_dataset_with_collecter_env_helper
+
+
+@pytest.fixture(name="dataset_id")
+def dataset_id():
+    return "cartpole-test-v0"
+
+
+@pytest.fixture(autouse=True)
+def createAndDestroyMinariDataset(dataset_id):
+    env = gym.make('CartPole-v1')
+    env = DataCollector(env, record_infos=True, max_buffer_steps=100000)
+
+    create_dummy_dataset_with_collecter_env_helper(
+        dataset_id, env, num_episodes=10
+    )
+
+    yield
+
+    minari.delete_dataset(dataset_id)
+
+
+def test_agile_create_dataset(dataset_id):
+    """
+    Tests that the AgileRL MinariToAgileDataset method works as expected.
+    """
+
+    dataset = MinariToAgileDataset(dataset_id)
+
+    assert isinstance(dataset, h5py.File)
+    assert dataset["observations"].size > 0
+    assert dataset["next_observations"].size > 0
+    assert dataset["actions"].size > 0
+    assert dataset["rewards"].size > 0
+    assert dataset["terminals"].size > 0
+
+
+def test_agile_create_buffer(dataset_id):
+    """
+    Tests that the AgileRL MinariToAgileBuffer method works as expected.
+    """
+
+    field_names = ["state", "action", "reward", "next_state", "done"]
+    memory = ReplayBuffer(action_dim=2,
+                          memory_size=10000,
+                          field_names=field_names,
+                          device="cpu")
+
+    assert memory.counter == 0
+
+    memory = MinariToAgileBuffer(dataset_id, memory)
+
+    assert isinstance(memory, ReplayBuffer)
+    assert memory.counter > 0
---
+++ tests/integrations/test_torch_rl.py
@@ -0,0 +1,29 @@
+import gymnasium as gym
+import pytest
+from torchrl.data.datasets.minari_data import MinariExperienceReplay
+
+
+def test_torch_minari_experience_replay():
+    """
+    Tests creation of the torchrl MinariExperienceReplay object.
+
+    According to their documentation, torchrl uses the MinariExperienceReplay
+    object as a way to download datasets from the remote Farama server.
+    """
+
+    batch_size = 32
+
+    dataset = MinariExperienceReplay("door-human-v1", batch_size=batch_size)
+    env = gym.make("AdroitHandDoor-v1")
+
+    sample = dataset.sample()
+
+    assert sample.batch_size[0] == batch_size
+    assert sample.shape[0] == batch_size
+
+    assert sample[0]['action'].shape == env.action_space.shape
+    assert sample[0]['observation'].shape == env.observation_space.shape
+
+    # Check that the dataset cannot be written to
+    with pytest.raises(RuntimeError):
+        dataset.empty()
