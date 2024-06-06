c68a741df99ed9ceae6300c4a5dd84efded51f21
Omar Younis <omar.younis98@gmail.com>
2024-06-06 19:43:23 +0700

    add docs

--- minari/storage/remotes/__init__.py
+++ minari/storage/remotes/__init__.py
@@ -1,3 +1,4 @@
+import os
 from typing import Dict, Type
 
 from minari.storage.remotes.cloud_storage import CloudStorage
@@ -5,10 +6,14 @@ from minari.storage.remotes.cloud_storage import CloudStorage
 from .gcp import GCPStorage
 
 
+DEFAULT_REMOTE = "gcp://minari-datasets"
+
 registry: Dict[str, Type[CloudStorage]] = {
     "gcp": GCPStorage
 }
 
 
 def get_cloud_storage(key_path=None) -> CloudStorage:
-    return registry["gcp"]("minari-datasets", key_path)
+    remote_spec = os.getenv("MINARI_REMOTE", DEFAULT_REMOTE)
+    cloud_type, name = remote_spec.split("://", maxsplit=1)
+    return registry[cloud_type](name, key_path)
