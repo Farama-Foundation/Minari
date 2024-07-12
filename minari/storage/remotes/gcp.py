2a629737d3c06a104d670a1ab6c7aae13a906c19
JosephCarrino <josephcarrinospam@gmail.com>
2024-07-12 19:28:28 +0200

    Fixed pre-commit error on cloud.storage

--- minari/storage/remotes/gcp.py
+++ minari/storage/remotes/gcp.py
@@ -6,7 +6,7 @@ from minari.storage.remotes.cloud_storage import CloudStorage
 
 
 try:
-    from google.cloud import storage
+    from google.cloud import storage as gcp_storage
     from tqdm import tqdm
 except ImportError:
     raise ImportError(
@@ -17,12 +17,12 @@ except ImportError:
 class GCPStorage(CloudStorage):
     def __init__(self, name: str, key_path: Optional[str] = None) -> None:
         if key_path is None:
-            self.storage_client = storage.Client.create_anonymous_client()
+            self.storage_client = gcp_storage.Client.create_anonymous_client()
         else:
-            self.storage_client = storage.Client.from_service_account_json(
+            self.storage_client = gcp_storage.Client.from_service_account_json(
                 json_credentials_path=key_path
             )
-        self.bucket = storage.Bucket(self.storage_client, name)
+        self.bucket = gcp_storage.Bucket(self.storage_client, name)
 
     def upload_path(self, path: Path, dataset_id: str) -> None:
         # See https://github.com/googleapis/python-storage/issues/27 for discussion on progress bars
