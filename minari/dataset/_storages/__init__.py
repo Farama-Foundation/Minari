c2b2cb870e23d1c008f85ca0d4239dd5091de556
JosephCarrino <josephcarrinospam@gmail.com>
2024-07-13 21:56:58 +0200

    Attempting to get dynamic optional storages imports to avoid ImportError

--- minari/dataset/_storages/__init__.py
+++ minari/dataset/_storages/__init__.py
@@ -1,12 +1,39 @@
-from typing import Dict, Type
+from typing import Callable, Dict, Type
 
 from minari.dataset.minari_storage import MinariStorage
 
-from .arrow_storage import ArrowStorage
-from .hdf5_storage import HDF5Storage
+
+def get_arrow_storage() -> Type[MinariStorage]:
+    from .arrow_storage import ArrowStorage
+
+    return ArrowStorage
 
 
-registry: Dict[str, Type[MinariStorage]] = {
-    HDF5Storage.FORMAT: HDF5Storage,
-    ArrowStorage.FORMAT: ArrowStorage,
+def get_hdf5_storage() -> Type[MinariStorage]:
+    from .hdf5_storage import HDF5Storage
+
+    return HDF5Storage
+
+
+registry_factory: Dict[str, Callable[[], Type[MinariStorage]]] = {
+    "arrow": get_arrow_storage,
+    "hdf5": get_hdf5_storage,
 }
+
+
+class RegistryDict(dict):
+    def __getitem__(self, key: str) -> Type[MinariStorage]:
+        # Custom behavior
+        if key in self:
+            value = super().__getitem__(key)
+            return value
+        else:
+            try:
+                value = registry_factory[key]()
+                self[key] = value
+                return value
+            except KeyError:
+                raise KeyError(f"Storage type {key} not supported")
+
+
+registry: RegistryDict = RegistryDict()
--- minari/storage/remotes/__init__.py
+++ minari/storage/remotes/__init__.py
@@ -3,15 +3,14 @@ from typing import Dict, Type
 
 from minari.storage.remotes.cloud_storage import CloudStorage
 
-from .gcp import GCPStorage
-
 
 DEFAULT_REMOTE = "gcp://minari-datasets"
 
-registry: Dict[str, Type[CloudStorage]] = {"gcp": GCPStorage}
-
 
 def get_cloud_storage(key_path=None) -> CloudStorage:
+    from .gcp import GCPStorage
+
+    registry: Dict[str, Type[CloudStorage]] = {"gcp": GCPStorage}
     remote_spec = os.getenv("MINARI_REMOTE", DEFAULT_REMOTE)
     cloud_type, name = remote_spec.split("://", maxsplit=1)
     return registry[cloud_type](name, key_path)
