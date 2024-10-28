4c301829c6a15b0a4510511f05552464645c9d2b
Omar Younis <omar.younis98@gmail.com>
2024-10-28 13:01:04 +0100

    try install reqs withing gif generation env fork

--- docs/_scripts/gen_dataset_md.py
+++ docs/_scripts/gen_dataset_md.py
@@ -78,11 +78,13 @@ def _generate_dataset_page(dataset_id, metadata):
     description = metadata.get("description")
     try:
         requirements = metadata.get("requirements", [])
-        for req in requirements:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", req])
+        install_reqs_cmd = ""
+        if len(requirements) > 0:
+            install_reqs_cmd = f"{sys.executable} -m pip install {' '.join(requirements)} &&"
         minari.download_dataset(dataset_id)
         subprocess.check_call(
             [
+                install_reqs_cmd,
                 sys.executable,
                 generate_gif.__file__,
                 f"--dataset_id={dataset_id}",
